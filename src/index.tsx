b2e3b1790d3f419c531e21950744a6a4d4f49889
syazwan <syazwanhosen@gmail.com>
2025-05-07 10:54:54 +0800

    styles: create a theme to use custom primary color

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#673ab7',
+            contrastText: '#ffffff',
+        },
+    },
+});
+
+export default theme;
