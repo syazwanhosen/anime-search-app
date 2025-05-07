565be0c2f48bc84db7a7e6ca4af39ac94777018e
syazwan <syazwanhosen@gmail.com>
2025-05-07 10:48:48 +0800

    chore: added button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button as MuiButton, ButtonProps as MuiButtonProps } from '@mui/material';
+
+// Types
+import { ButtonProps } from '../../types';
+
+const Button: React.FC<ButtonProps> = ({ label, ...props }) => {
+    return (
+        <MuiButton variant="contained" color="primary" {...props}>
+            {label}
+        </MuiButton>
+    );
+};
+
+export default Button;
